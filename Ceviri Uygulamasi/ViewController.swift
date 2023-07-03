56cc5f5424b8dfb28a94db770c730b1390bb0c68
yunusdvp <yunusemre7033@gmail.com>
2023-07-03 09:04:11 +0300

    Initial Commit

---
+++ Ceviri Uygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ceviri Uygulamasi
+//
+//  Created by Yunus Emre ÖZŞAHİN on 3.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
